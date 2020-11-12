0186aebb4c9ab953ca029eae1e24bb33c79cbcb0
AsfanUlla <asfanulla@gmail.com>
2020-11-13 01:17:32 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.IO;
+using Microsoft.ML;
+using Microsoft.ML.Trainers;
+
+namespace ProjectRecommender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MLContext mlContext = new MLContext();
+            (IDataView trainingDataView, IDataView testDataView) = LoadData(mlContext);
+            ITransformer model = BuildAndTrainModel(mlContext, trainingDataView);
+            EvaluateModel(mlContext, testDataView, model);
+            UseModelForSinglePrediction(mlContext, model);
+            SaveModel(mlContext, trainingDataView.Schema, model);
+        }
+
+        public static (IDataView training, IDataView test) LoadData(MLContext mlContext)
+        {
+            var trainingDataPath = Path.Combine(Environment.CurrentDirectory, "Data", "recommendation-ratings-train.csv");
+            var testDataPath = Path.Combine(Environment.CurrentDirectory, "Data", "recommendation-ratings-test.csv");
+
+            IDataView trainingDataView = mlContext.Data.LoadFromTextFile<ProjectRating>(trainingDataPath, hasHeader: true, separatorChar: ',');
+            IDataView testDataView = mlContext.Data.LoadFromTextFile<ProjectRating>(testDataPath, hasHeader: true, separatorChar: ',');
+
+            return (trainingDataView, testDataView);
+        }
+
+        public static ITransformer BuildAndTrainModel(MLContext mlContext, IDataView trainingDataView)
+        {
+            IEstimator<ITransformer> estimator = mlContext.Transforms.Conversion.MapValueToKey(outputColumnName: "userIdEncoded", inputColumnName: "userId")
+                .Append(mlContext.Transforms.Conversion.MapValueToKey(outputColumnName: "projectIdEncoded", inputColumnName: "projectId"));
+
+            var options = new MatrixFactorizationTrainer.Options
+            {
+                MatrixColumnIndexColumnName = "userIdEncoded",
+                MatrixRowIndexColumnName = "projectIdEncoded",
+                LabelColumnName = "Label",
+                NumberOfIterations = 20,
+                ApproximationRank = 100
+            };
+
+            var trainerEstimator = estimator.Append(mlContext.Recommendation().Trainers.MatrixFactorization(options));
+
+            Console.WriteLine("=============== Training the model ===============");
+            ITransformer model = trainerEstimator.Fit(trainingDataView);
+
+            return model;
+        }
+
+        public static void EvaluateModel(MLContext mlContext, IDataView testDataView, ITransformer model)
+        {
+            Console.WriteLine("=============== Evaluating the model ===============");
+            var prediction = model.Transform(testDataView);
+            var metrics = mlContext.Regression.Evaluate(prediction, labelColumnName: "Label", scoreColumnName: "Score");
+            Console.WriteLine("Root Mean Squared Error : " + metrics.RootMeanSquaredError.ToString());
+            Console.WriteLine("RSquared: " + metrics.RSquared.ToString());
+        }
+
+        public static void UseModelForSinglePrediction(MLContext mlContext, ITransformer model)
+        {
+            Console.WriteLine("=============== Making a prediction ===============");
+            var predictionEngine = mlContext.Model.CreatePredictionEngine<ProjectRating, ProjectRatingPrediction>(model);
+
+            var testInput = new ProjectRating { userId = 6, projectId = 10 };
+            var projectRatingPrediction = predictionEngine.Predict(testInput);
+
+            if (Math.Round(projectRatingPrediction.Score, 1) > 3.5)
+            {
+                Console.WriteLine("Project " + testInput.projectId + " is recommended for user " + testInput.userId);
+            }
+            else
+            {
+                Console.WriteLine("Project " + testInput.projectId + " is not recommended for user " + testInput.userId);
+            }
+
+        }
+
+        public static void SaveModel(MLContext mlContext, DataViewSchema trainingDataViewSchema, ITransformer model)
+        {
+            var modelPath = Path.Combine(Environment.CurrentDirectory, "Data", "ProjectRecommenderModel.zip");
+
+            Console.WriteLine("=============== Saving the model to a file ===============");
+            mlContext.Model.Save(model, trainingDataViewSchema, modelPath);
+        }
+
+    }
+}
---
+++ ProjectRatingData.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.ML.Data;
+
+namespace ProjectRecommender
+{
+    public class ProjectRating
+    {
+        [LoadColumn(0)]
+        public float userId;
+        [LoadColumn(1)]
+        public float projectId;
+        [LoadColumn(2)]
+        public float Label;
+    }
+
+    public class ProjectRatingPrediction
+    {
+        public float Label;
+        public float Score;
+    }
+}
